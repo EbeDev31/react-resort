50a0d68cd1c781343f5e28e3599cc235885d556f
ebedev31 <ebedev31@gmail.com>
2020-05-20 17:19:17 +0200

    added webpack config for images

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,13 @@ module.exports={
             {
                 test:/\.css$/,
                 use:['style-loader','css-loader']
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                'file-loader',
+                ],
+            },
         ]
     },
     devServer:{
